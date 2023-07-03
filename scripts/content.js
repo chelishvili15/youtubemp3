96064994e9c8165719cc3dba47e69152b5d24c32
chelishvili15 <chelo@redberry.ge>
2023-07-03 17:02:13 +0400

    feat: connet on frontend app

--- scripts/content.js
+++ scripts/content.js
@@ -1,16 +1,5 @@
-var options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '69f56c9d5dmsh47a93b89b474344p121e24jsnec66aa328a1e',
-		'X-RapidAPI-Host': 'youtube-mp3-download1.p.rapidapi.com'
-	}
-};
+var url = 'https://bulbuli.netlify.app/download/'
 
-var url = 'https://youtube-mp3-download1.p.rapidapi.com/dl?id='
+var id = location.href.replace('https://www.youtube.com/watch?v=', '').slice(0, 11)
 
-var id = location.href.replace('https://www.youtube.com/watch?v=', '')
-
-fetch(`${url}${id}`, options)
-	.then(response => response.json())
-	.then(response => window.open(response.link, '_blank'))
-	.catch(err => console.error(err));
+window.open(`${url}${id}`, '_blank')
